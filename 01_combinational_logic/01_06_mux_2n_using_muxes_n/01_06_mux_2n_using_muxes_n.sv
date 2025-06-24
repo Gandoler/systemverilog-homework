be7ba43f85e5c2dac3014fac6951717803dcc2d2
Gleb Frolov <81429241+Gandoler@users.noreply.github.com>
2025-06-24 13:02:34 +0300

    Update 01_06_mux_2n_using_muxes_n.sv

--- 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
+++ 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
@@ -27,5 +27,31 @@ module mux_4_1
   // Task:
   // Implement mux_4_1 using three instances of mux_2_1
 
+  wire [3:0]y0, y1;
+  
+  
+   mux_2_1 right_two(
+  .d0(d0), //00
+  .d1(d1), //01
+  .sel(sel[0]),
+  .y(y0)
+  );
+  
+  mux_2_1 left_two(
+  .d0(d2), //10
+  .d1(d3), //11
+  .sel(sel[0]),
+  .y(y1)
+  );
+ 
+  
+  mux_2_1 final_choice(
+  .d0(y0), 
+  .d1(y1), 
+  .sel(sel[1]),
+  .y(y)
+  );
+  
 
+  
 endmodule
