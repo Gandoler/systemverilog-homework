7f15ce362357b60d1a31bfd59d58251192d8da84
Gleb Frolov <81429241+Gandoler@users.noreply.github.com>
2025-06-24 13:41:16 +0300

    Update 01_09_and_gate_using_mux.sv

--- 01_combinational_logic/01_09_and_gate_using_mux/01_09_and_gate_using_mux.sv
+++ 01_combinational_logic/01_09_and_gate_using_mux/01_09_and_gate_using_mux.sv
@@ -28,5 +28,15 @@ module and_gate_using_mux
   // Implement and gate using instance(s) of mux,
   // constants 0 and 1, and wire connections
 
+  wire const0 = 1'b0;
 
+  mux mux_a(
+  .d0(const0), 
+  .d1(b),
+  .sel(a),
+  .y(o)
+  );
+
+
+  
 endmodule
