77b403685614b60669aac99783f07f0230d65030
Gleb Frolov <81429241+Gandoler@users.noreply.github.com>
2025-06-25 11:23:41 +0300

    Update 01_11_xor_gate_using_mux.sv

--- 01_combinational_logic/01_11_xor_gate_using_mux/01_11_xor_gate_using_mux.sv
+++ 01_combinational_logic/01_11_xor_gate_using_mux/01_11_xor_gate_using_mux.sv
@@ -32,41 +32,59 @@ module xor_gate_using_mux
   wire const0 = 1'b0;
   
   wire na, nb, a_and_nb, b_and_na;
+
+ mux mux_not(
+    .d0(const1), 
+    .d1(const0),
+    .sel(a),
+    .y(na)
+   );
+   
+  
+  mux mux_o(
+    .d0(a), 
+    .d1(na),
+    .sel(b),
+    .y(o)
+  );
+
+
+
   
   // not
-  mux mux_not_a(
-  .d0(const1), 
-  .d1(const0),
-  .sel(a),
-  .y(na)
-  ); 
-  mux mux_not_b(
-  .d0(const1), 
-  .d1(const0),
-  .sel(b),
-  .y(nb)
-  ); 
+//   mux mux_not_a(
+//   .d0(const1), 
+//   .d1(const0),
+//   .sel(a),
+//   .y(na)
+//   ); 
+//   mux mux_not_b(
+//   .d0(const1), 
+//   .d1(const0),
+//   .sel(b),
+//   .y(nb)
+//   ); 
   
-  //and
-   mux mux_a_and_nb(
-  .d0(const0), 
-  .d1(nb),
-  .sel(a),
-  .y(a_and_nb)
-  );
-   mux mux_b_and_na(
-  .d0(const0), 
-  .d1(b),
-  .sel(na),
-  .y(b_and_na)
-  );
+//   //and
+//    mux mux_a_and_nb(
+//   .d0(const0), 
+//   .d1(nb),
+//   .sel(a),
+//   .y(a_and_nb)
+//   );
+//    mux mux_b_and_na(
+//   .d0(const0), 
+//   .d1(b),
+//   .sel(na),
+//   .y(b_and_na)
+//   );
   
-//or
-  mux mux_first_or_second(
-  .d0(b_and_na), 
-  .d1(const1),
-  .sel(a_and_nb),
-  .y(o)
-  );
+// //or
+//   mux mux_first_or_second(
+//   .d0(b_and_na), 
+//   .d1(const1),
+//   .sel(a_and_nb),
+//   .y(o)
+//   );
   
 endmodule
