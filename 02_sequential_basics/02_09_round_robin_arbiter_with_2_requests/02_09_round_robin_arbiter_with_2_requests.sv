1fcfe0146c7236e5211af2e37541ea76e60200d6
Gleb Frolov <81429241+Gandoler@users.noreply.github.com>
2025-06-26 12:30:20 +0300

    Update 02_09_round_robin_arbiter_with_2_requests.sv

--- 02_sequential_basics/02_09_round_robin_arbiter_with_2_requests/02_09_round_robin_arbiter_with_2_requests.sv
+++ 02_sequential_basics/02_09_round_robin_arbiter_with_2_requests/02_09_round_robin_arbiter_with_2_requests.sv
@@ -22,51 +22,18 @@ module round_robin_arbiter_with_2_requests
     // Example:
     // requests -> 01 00 10 11 11 00 11 00 11 11
     // grants   -> 01 00 10 01 10 00 01 00 10 01
-    enum logic[1:0]
-  {
-    non_request   =00,
-    f_request     =01,
-    s_request     =10,
-    t_request     =11
-  }
- next_in_line, current_request;
-  
-  
-  
+    logic  [1:0] prev;
     
-    always_comb begin 
-      next_in_line = current_request;
+   
     
-      case (current_request)
-        non_request: begin
-          if(requests == 01) next_in_line = f_request;
-          if(requests == 10) next_in_line = s_request;
-          if(requests == 11) next_in_line = t_request;
-          end
-        f_request:  begin
-          if(requests == 11) next_in_line = s_request;
-          if(requests == 00) next_in_line = non_request;
-          end
-        s_request:  begin
-          if(requests == 11) next_in_line = f_request;
-          if(requests == 00) next_in_line = non_request;
-          end
-        t_request:  begin
-          if(requests == 00) next_in_line = non_request;
-          if(requests == 01) next_in_line = f_request;
-          if(requests == 10) next_in_line = s_request;
-          end
-         
-      endcase 
-    end
-
-
+    assign grants =  (&requests) ? (prev=='b10 ? 01:10 )  : requests;
 
     always_ff @(posedge clk) begin
       if (rst) 
-        current_request <= non_request;
-      else 
-        current_request <= next_in_line;
+        prev <= 'b0;
+      else
+        prev <= requests;
+    
     end
     
     
