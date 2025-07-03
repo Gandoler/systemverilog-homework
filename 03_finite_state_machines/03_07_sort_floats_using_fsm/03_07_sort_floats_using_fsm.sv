03503c70b0c64f73c11df129d47570ace1642e3f
Gleb Frolov <81429241+Gandoler@users.noreply.github.com>
2025-07-03 11:54:55 +0300

    Update 03_07_sort_floats_using_fsm.sv

--- 03_finite_state_machines/03_07_sort_floats_using_fsm/03_07_sort_floats_using_fsm.sv
+++ 03_finite_state_machines/03_07_sort_floats_using_fsm/03_07_sort_floats_using_fsm.sv
@@ -35,6 +35,91 @@ module sort_floats_using_fsm (
     //
     // The FLEN parameter is defined in the "import/preprocessed/cvw/config-shared.vh" file
     // and usually equal to the bit width of the double-precision floating-point number, FP64, 64 bits.
+     enum logic[2:0]
+      {
+        Send_a_b             = 3'd0,
+        Send_b_c_get_f_res   = 3'd1,
+        Send_c_d_get_s_res   = 3'd2,
+        Get_t_res            = 3'd3
+      } state, new_state;
+      logic [2:0]e1e2e3_comare, errors;
+      
+    
+    always_comb begin 
+      new_state = state;
+      
+      case(state)
+        Send_a_b           : if(valid_in) new_state = Send_b_c_get_f_res;
+        Send_b_c_get_f_res : if(valid_in) new_state = Send_c_d_get_s_res;
+        Send_c_d_get_s_res : if(valid_in) new_state = Get_t_res;
+        Get_t_res          :              new_state = Send_a_b;
+        endcase
+    end
+    
+     always_comb begin
+      f_le_a ='x;
+      f_le_b ='x;
+      case(state)
+        Send_a_b: begin
+          f_le_a           = unsorted[0];
+          f_le_b           = unsorted[1];
+        end
+        
+        Send_b_c_get_f_res : begin
+          e1e2e3_comare[2] = f_le_res;
+          errors[2]        = f_le_err;
+          f_le_a           = unsorted[1];
+          f_le_b           = unsorted[2];
+        end
+        
+        Send_c_d_get_s_res : begin
+          e1e2e3_comare[1] = f_le_res;
+          errors[1]        = f_le_err;
+          f_le_a           = unsorted[2];
+          f_le_b           = unsorted[3];
+        end
+        
+        Get_t_res          : begin
+          e1e2e3_comare[0] = f_le_res;
+          errors[0]        = f_le_err;
+        end
+        endcase
+    end  
+
+    assign err = | errors ;
+    
+    always_comb begin
+      if(state == Get_t_res & ~err) begin 
+        if (e1e2e3_comare[1])
+         if (e1e2e3_comare[0])
+           sorted = unsorted;
+         else
+           if (e1e2e3_comare[2])
+             {   sorted [0],   sorted [1], sorted [2] } = { unsorted [1], unsorted [0], unsorted[2] };
+           else 
+             {   sorted [0],   sorted [1], sorted [2] } = { unsorted [1], unsorted [2], unsorted[0] };
+       else
+         if (e1e2e3_comare[0])
+           if (e1e2e3_comare[2])
+             {   sorted [0],   sorted [1], sorted [2] } = { unsorted [0], unsorted [2], unsorted[1] };
+           else 
+             {   sorted [0],   sorted [1], sorted [2] } = { unsorted [2], unsorted [0], unsorted[1] };
+         else
+           {   sorted [0],   sorted [1], sorted [2] } = { unsorted [2], unsorted [1], unsorted[0] };
+        valid_out = 'b1 ;
+      end
+    
+    end
+
+
+
+    always_ff @(posedge clk) begin
+      if(rst)
+        state = Send_a_b;
+      else
+        state = new_state;
+    end
+
 
 
 
