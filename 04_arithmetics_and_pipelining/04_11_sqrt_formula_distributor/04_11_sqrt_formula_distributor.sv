e24a14db5261dcd265cfabff8f38862142e58af7
Gleb Frolov <81429241+Gandoler@users.noreply.github.com>
2025-07-07 12:35:51 +0300

    Update 04_11_sqrt_formula_distributor.sv

--- 04_arithmetics_and_pipelining/04_11_sqrt_formula_distributor/04_11_sqrt_formula_distributor.sv
+++ 04_arithmetics_and_pipelining/04_11_sqrt_formula_distributor/04_11_sqrt_formula_distributor.sv
@@ -42,6 +42,119 @@ module sqrt_formula_distributor
     // Hint:
     // Instantiate sufficient number of "formula_1_impl_1_top", "formula_1_impl_2_top",
     // or "formula_2_top" modules to achieve desired performance.
+     localparam N = 50;
+    
+    logic [6:0]   counter;
+    logic [N-1:0] idx_array;
+    
+    logic [31:0]  a_array         [0 : N-1];
+    logic [31:0]  b_array         [0 : N-1];
+    logic [31:0]  c_array         [0 : N-1];
+    logic [N-1:0] vld_array;
+    
+    
+    logic [31:0]  res_array       [0:N-1];
+    logic [N-1:0] res_vld_array;
+
+    
+    
+    always_ff @(posedge clk) begin
+      if(rst) counter        <= 'b0;
+      else if(arg_vld) begin
+        counter              <= (counter == N-1) ? 'b0: counter + 'b1;
+        idx_array[counter-1] <= 0;
+        idx_array[counter]   <= 1;
+      end
+    end
+    
+    
+    always_ff @(posedge clk) begin
+      if(arg_vld & idx_array[counter]) begin
+        a_array[counter] <= a;  
+        b_array[counter] <= b;
+        c_array[counter] <= c;
+      end
+    end
+    
+    
+    always_ff @(posedge clk) begin
+      if (rst) begin
+        vld_array <= 'b0;
+      end
+      else if(idx_array[counter]) begin
+        vld_array[counter-1] <= 0;
+        vld_array[counter]   <= 1;
+      end
+    end
+    
+    genvar i;
+    
+    generate 
+      for(i = 0; i < N; i++) begin
+        if (formula == 1) begin
+          if (impl == 1)begin
+            formula_1_impl_1_top formula_1_impl_1_top_i
+            (
+               .clk(clk),
+               .rst(rst),
+               
+               .arg_vld(vld_array[i]),
+               .a(a_array[i]),
+               .b(b_array[i]),
+               .c(c_array[i]),
+               
+               .res_vld(res_vld_array[i]),
+               .res(res_array[i])
+            );
+          
+          end else if (impl == 2) begin
+            formula_1_impl_2_top formula_1_impl_2_top_i
+            (
+                .clk(clk),
+                .rst(rst),
+                
+                .arg_vld(vld_array[i]),
+                .a(a_array[i]),
+                .b(b_array[i]),
+                .c(c_array[i]),
+                
+                .res_vld(res_vld_array[i]),
+                .res(res_array[i])
+            );
+        
+          end
+          
+        end else if (formula == 2) begin
+          formula_2_top formula_2_top_i
+          (
+              .clk(clk),
+              .rst(rst),
+              
+              .arg_vld(vld_array[i]),
+              .a(a_array[i]),
+              .b(b_array[i]),
+              .c(c_array[i]),
+              
+              .res_vld(res_vld_array[i]),
+              .res(res_array[i])
+          );
+        
+        end
+      end
+    endgenerate 
+    
+    
+    assign res_vld = | res_vld_array;
+    logic res_comb;
+    assign res = res_comb;
+    
+    always_comb begin
+      for(int j = 0; j < N; j++ ) begin
+        if (res_vld_array[j]) begin
+          res_comb = res_array[j];
+        end
+      end
+    end
 
 
 endmodule
